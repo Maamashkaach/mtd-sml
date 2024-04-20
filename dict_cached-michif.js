4fdabce2dffb93ea10d155b5c57f074cb83d95b8
Maamashkaach <maamashkaach@gmail.com>
2024-04-20 08:51:33 -0700

    Setup of new dictionary

---
+++ dict_cached-michif.js
@@ -0,0 +1,1 @@
+var dataDict = [{"source": "data", "word": "hello", "definition": "bonjour", "sorting_form": [7, 4, 11, 11, 14], "entryID": "0"}, {"source": "data", "word": "test", "definition": "data", "sorting_form": [19, 4, 18, 19], "entryID": "1"}]
